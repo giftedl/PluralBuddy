9e4cde463bf170aeaaadc878aedc00df189b8a87
dvelo <52332868+DeveloLongScript@users.noreply.github.com>
2025-11-10 20:12:13 -0600

    add guild members intent

--- packages/bot/seyfert.config.ts
+++ packages/bot/seyfert.config.ts
@@ -8,5 +8,5 @@ export default config.bot({
         components: "components",
         events: "events"
     },
-    intents: ["Guilds", "MessageContent", "GuildMessages", "GuildWebhooks"]
+    intents: ["Guilds", "MessageContent", "GuildMessages", "GuildWebhooks", "GuildMembers"]
 });
