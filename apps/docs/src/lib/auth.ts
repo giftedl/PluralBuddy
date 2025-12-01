a9e2f6fa18d86ad0b9fc098d3089b82881cfc4ff
dvelo <52332868+DeveloLongScript@users.noreply.github.com>
2025-11-30 18:37:48 -0600

    (docs) finish rest of terms

--- apps/docs/src/lib/auth.ts
+++ apps/docs/src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 import { oidcProvider } from "better-auth/plugins";
+import { RESTGetAPICurrentUserResult } from "discord-api-types/v10"
 
 const client = new MongoClient(process.env.MONGO ?? "");
 
@@ -12,6 +13,43 @@ export const auth = betterAuth({
 			enabled: true,
 			clientId: process.env.CLIENT_ID as string,
 			clientSecret: process.env.CLIENT_SECRET as string,
+			disableDefaultScope: true,
+			scope: ["identify"],
+			getUserInfo: async (token) => {
+
+				// Custom implementation to get user info
+				const response = await fetch("https://discord.com/api/v10/users/@me", {
+					headers: {
+						Authorization: `Bearer ${token.accessToken}`,
+					},
+				});
+
+				let imageUrl = null;
+
+				const profile: RESTGetAPICurrentUserResult = await response.json();
+				if (profile.avatar === null) {
+					const defaultAvatarNumber =
+						profile.discriminator === "0"
+							? Number(BigInt(profile.id) >> BigInt(22)) % 6
+							: parseInt(profile.discriminator) % 5;
+					imageUrl = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`;
+				} else {
+					const format = profile.avatar.startsWith("a_") ? "gif" : "png";
+					imageUrl = `https://cdn.discordapp.com/avatars/${profile.id}/${profile.avatar}.${format}`;
+				}
+
+				return {
+					user: {
+						id: profile.id,
+						name: profile.username,
+						email: `${profile.id}@redacted.giftedly.dev`,
+						image: imageUrl,
+						// Yep, thats totally the real email. Trust me.
+						emailVerified: true,
+					},
+					data: profile,
+				};
+			},
 		},
 	},
 	plugins: [
@@ -24,18 +62,18 @@ export const auth = betterAuth({
 				"profile",
 				"openid",
 				"email",
-                "offline_access",
+				"offline_access",
 				"alter:read",
 				"alter:write",
-                "tags:read",
-                "tags:write",
-                "tags:alters",
+				"tags:read",
+				"tags:write",
+				"tags:alters",
 				"system:read",
 				"system:write",
 				"system:admin",
 			],
-            defaultScope: "profile"
+			defaultScope: "profile"
 		}),
 	],
-	disabledPaths: ["/oauth2/register"],
+	disabledPaths: ["/oauth2/register"]
 });
