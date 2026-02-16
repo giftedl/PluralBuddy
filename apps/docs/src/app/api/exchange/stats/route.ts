1e85d459cc072c762d272ae8febb4ad8852ccc8a
giftedl <52332868+giftedl@users.noreply.github.com>
2026-02-16 05:34:11 -0600

    remove useless route args

--- apps/docs/src/app/api/exchange/stats/route.ts
+++ apps/docs/src/app/api/exchange/stats/route.ts
@@ -1,10 +1,7 @@
 import { api } from "@/lib/rpc";
 import { NextRequest } from "next/server";
 
-export async function GET(
-	request: NextRequest,
-	{ params }: { params: Promise<{ user: string }> },
-) {
+export async function GET() {
 	const response = await api.stats.$get();
 
 	return response;
