ab7c3c8b9357480970280841950d24ce4d8c851c
giftedl <52332868+giftedl@users.noreply.github.com>
2026-02-16 11:41:12 -0600

    Disable Typescript checking

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,11 +1,15 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  reactStrictMode: true,
-  serverExternalPackages: ['mongodb']
+	reactStrictMode: true,
+	serverExternalPackages: ["mongodb"],
+	typescript: {
+		// TypeScript is strictly for IDE support for the frontend.
+		ignoreBuildErrors: true,
+	},
 };
 
 export default withMDX(config);
