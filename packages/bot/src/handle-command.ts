5ed77f153b8e0d015399b8a291ce78dff0276a4d
giftedl <52332868+giftedl@users.noreply.github.com>
2026-02-25 19:25:12 -0600

    add yuna as command resolver

--- packages/bot/src/handle-command.ts
+++ packages/bot/src/handle-command.ts
@@ -9,6 +9,20 @@ import { DiscordSnowflake } from "@sapphire/snowflake";
 import { posthogClient } from ".";
 
 export default class PluralBuddyHandleCommand extends HandleCommand {
-    override argsParser = Yuna.parser();
+	override argsParser = Yuna.parser();
 
+	override resolveCommandFromContent = Yuna.resolver({
+		/**
+		 * You need to pass the client in order to prepare the commands that the resolver will use.
+		 */
+		client: this.client,
+		/**
+		 * Event to be emitted each time the commands are prepared.
+		 */
+		afterPrepare: (metadata) => {
+			this.client.logger.debug(
+				`Ready to use ${metadata.commands.length} commands !`,
+			);
+		},
+	});
 }
