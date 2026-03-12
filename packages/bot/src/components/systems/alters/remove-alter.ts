66725b5816d3154e0ffa290cdace6543f22d54ef
giftedl <52332868+giftedl@users.noreply.github.com>
2026-03-12 17:33:11 -0500

    Add pagination button to removing alters

--- packages/bot/src/components/systems/alters/remove-alter.ts
+++ packages/bot/src/components/systems/alters/remove-alter.ts
@@ -1,49 +1,74 @@
 /**  * PluralBuddy Discord Bot  *  - is licensed under MIT License.  */
 
-import { ComponentCommand, type ComponentContext } from "seyfert";
+import {
+	ActionRow,
+	Button,
+	ComponentCommand,
+	type ComponentContext,
+} from "seyfert";
 import { InteractionIdentifier } from "@/lib/interaction-ids";
 import { alterCollection, userCollection } from "@/mongodb";
 import { AlertView } from "@/views/alert";
-import { MessageFlags } from "seyfert/lib/types";
+import { ButtonStyle, MessageFlags } from "seyfert/lib/types";
 import { getUserById, writeUserById } from "@/types/user";
+import { emojis } from "@/lib/emojis";
 
 export default class RemoveAlterButton extends ComponentCommand {
-   componentType = 'Button' as const;
-   
-   override filter(context: ComponentContext<typeof this.componentType>) {
-       return InteractionIdentifier.Systems.Configuration.Alters.RemoveAlterConfirm.startsWith(context.customId)
-   }
+	componentType = "Button" as const;
 
-    override async run(ctx: ComponentContext<typeof this.componentType>) {
-        const alterId = InteractionIdentifier.Systems.Configuration.Alters.RemoveAlterConfirm.substring(
-            ctx.customId,
-        )[0];
+	override filter(context: ComponentContext<typeof this.componentType>) {
+		return InteractionIdentifier.Systems.Configuration.Alters.RemoveAlterConfirm.startsWith(
+			context.customId,
+		);
+	}
 
-        const systemId = ctx.author.id;
-        const query = alterCollection.findOne({
+	override async run(ctx: ComponentContext<typeof this.componentType>) {
+		const alterId =
+			InteractionIdentifier.Systems.Configuration.Alters.RemoveAlterConfirm.substring(
+				ctx.customId,
+			)[0];
+
+		const systemId = ctx.author.id;
+		const query = alterCollection.findOne({
 			$and: [{ alterId: Number(alterId) }, { systemId }],
-        });
-        const alter = await query;
+		});
+		const alter = await query;
 
-        if (alter === null) {
-            return await ctx.write({
-                components: new AlertView(ctx.userTranslations()).errorView("ERROR_ALTER_DOESNT_EXIST"),
-                flags: MessageFlags.Ephemeral + MessageFlags.IsComponentsV2
-            })
-        }
+		if (alter === null) {
+			return await ctx.write({
+				components: new AlertView(ctx.userTranslations()).errorView(
+					"ERROR_ALTER_DOESNT_EXIST",
+				),
+				flags: MessageFlags.Ephemeral + MessageFlags.IsComponentsV2,
+			});
+		}
 
-        await alterCollection.findOneAndDelete(
-            { alterId: Number(alter.alterId), systemId },
-        );
-        
-        await userCollection.updateOne(
-            { userId: ctx.author.id },
-            { $pull: { "system.alterIds": Number(alterId) } }
-        )
+		await alterCollection.findOneAndDelete({
+			alterId: Number(alter.alterId),
+			systemId,
+		});
 
-        return await ctx.update({
-            components: new AlertView(ctx.userTranslations()).successView("ALTER_DELETION_FINISHED"),
-            flags: MessageFlags.Ephemeral + MessageFlags.IsComponentsV2
-        })
-    }
+		await userCollection.updateOne(
+			{ userId: ctx.author.id },
+			{ $pull: { "system.alterIds": Number(alterId) } },
+		);
+
+		return await ctx.update({
+			components: [
+				...new AlertView(ctx.userTranslations()).successView(
+					"ALTER_DELETION_FINISHED",
+				),
+				new ActionRow().setComponents(
+					new Button()
+						.setEmoji(emojis.undo)
+						.setStyle(ButtonStyle.Secondary)
+						.setLabel(ctx.userTranslations().PAGINATION_PREVIOUS_PAGE)
+						.setCustomId(
+							InteractionIdentifier.Systems.Configuration.GeneralTab.Index.create(),
+						),
+				),
+			],
+			flags: MessageFlags.Ephemeral + MessageFlags.IsComponentsV2,
+		});
+	}
 }
