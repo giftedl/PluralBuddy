91ec74000111638d29ad24d000332b070388583a
giftedl <52332868+giftedl@users.noreply.github.com>
2026-03-13 21:59:21 -0500

    fix locations

--- apps/docs/src/lib/openapi.ts
+++ apps/docs/src/lib/openapi.ts
@@ -2,5 +2,5 @@ import { createOpenAPI } from 'fumadocs-openapi/server';
 
 export const openapi = createOpenAPI({
   // the OpenAPI schema, you can also give it an external URL.
-  input: ['http://localhost:3000/openapi.yml'],
+  input: ['public/openapi.yml'],
 });
