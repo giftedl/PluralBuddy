b8d9e6ccffa554fa4c217d70ad74424f5f25294c
giftedl <52332868+giftedl@users.noreply.github.com>
2026-03-20 23:14:37 -0700

    Defer reply

--- packages/bot/src/commands/who-asked.ts
+++ packages/bot/src/commands/who-asked.ts
@@ -15,7 +15,8 @@ import { MessageFlags } from "seyfert/lib/types";
 	guildId: process.env.BRANCH === "canary" ? [] : ["1077258761443483708", "1444187699924963350"],
 })
 export default class WhoAskedCommand extends Command {
-	override run(ctx: CommandContext) {
+	override async run(ctx: CommandContext) {
+        await ctx.deferReply();
 		return ctx.write({
 			components: [
 				new Container().setComponents(
