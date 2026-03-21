508c2891d59218d8848c11816919bad3784a2464
giftedl <52332868+giftedl@users.noreply.github.com>
2026-03-20 23:08:28 -0700

    Who asked and who knows

---
+++ packages/bot/src/commands/who-asked.ts
@@ -0,0 +1,37 @@
+import {
+	AttachmentBuilder,
+	Command,
+	CommandContext,
+	Container,
+	Declare,
+	MediaGallery,
+	MediaGalleryItem,
+} from "seyfert";
+import { MessageFlags } from "seyfert/lib/types";
+
+@Declare({
+	name: "who-asked",
+	description: "Like genuinely, who asked",
+	guildId: process.env.BRANCH === "canary" ? [] : ["1077258761443483708", "1444187699924963350"],
+})
+export default class WhoAskedCommand extends Command {
+	override run(ctx: CommandContext) {
+		return ctx.write({
+			components: [
+				new Container().setComponents(
+					new MediaGallery().setItems(
+						new MediaGalleryItem()
+							.setMedia("attachment://who_asked.mov")
+							.setDescription("Who asked!"),
+					),
+				),
+			],
+			files: [
+				new AttachmentBuilder()
+					.setName("who_asked.mov")
+					.setFile("path", "content/easter-eggs/who_asked.mov"),
+			],
+			flags: MessageFlags.IsComponentsV2 + MessageFlags.Ephemeral,
+		});
+	}
+}
---
+++ packages/bot/src/commands/who-knows.ts
@@ -0,0 +1,37 @@
+import {
+	AttachmentBuilder,
+	Command,
+	CommandContext,
+	Container,
+	Declare,
+	MediaGallery,
+	MediaGalleryItem,
+} from "seyfert";
+import { MessageFlags } from "seyfert/lib/types";
+
+@Declare({
+	name: "who-knows",
+	description: "Who knows!",
+	guildId: process.env.BRANCH === "canary" ? [] : ["1077258761443483708", "1444187699924963350"],
+})
+export default class WhoAskedCommand extends Command {
+	override run(ctx: CommandContext) {
+		return ctx.write({
+			components: [
+				new Container().setComponents(
+					new MediaGallery().setItems(
+						new MediaGalleryItem()
+							.setMedia("attachment://who_knows_v3.mov")
+							.setDescription("Who knows!"),
+					),
+				),
+			],
+			files: [
+				new AttachmentBuilder()
+					.setName("who_knows_v3.mov")
+					.setFile("path", "content/easter-eggs/who_knows_v3.mov"),
+			],
+			flags: MessageFlags.IsComponentsV2 + MessageFlags.Ephemeral,
+		});
+	}
+}
