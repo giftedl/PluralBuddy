45eb9e0d9c73c4fcc98a89690c05849286ca9407
giftedl <52332868+giftedl@users.noreply.github.com>
2026-03-29 15:28:42 -0500

    Do time testing on i18n

--- packages/bot/src/lib/lang.ts
+++ packages/bot/src/lib/lang.ts
@@ -5,6 +5,7 @@ import { CacheFrom } from "seyfert";
 export const langMemoryCache: Record<string, string> = {};
 
 export async function language<T extends boolean | undefined>(id: string, noCache?: T): Promise<T extends true ? null | string : string> {
+	console.time("lang")
 	try {
 		let data = langMemoryCache[id] ?? (await client.cache.i18n.get(id))?.l;
 
@@ -21,9 +22,11 @@ export async function language<T extends boolean | undefined>(id: string, noCach
 			}
 		}
 
+		console.timeEnd("lang")
 		return data;
 	} catch (e) {
 		console.error(e)
+		console.timeEnd("lang")
 		if (noCache)
 			return null as T extends true ? null | string : string;
 		return "en";
