28ecdb2644dfcc07716ea32cb66f0d11bf2b4c90
giftedl <52332868+giftedl@users.noreply.github.com>
2026-04-01 21:48:55 -0700

    Add message cache flush

---
+++ packages/bot/src/commands/debug/flush-message-cache.ts
@@ -0,0 +1,25 @@
+import { AlertView } from "@/views/alert";
+import { CommandContext, Declare, SubCommand } from "seyfert";
+import { MessageFlags } from "seyfert/lib/types";
+
+@Declare({
+    name: "flush-msg-cache",
+    description: "Flush legacy message cache",
+    guildId: ["1444187699924963350"],
+})
+export default class FlushMessageCache extends SubCommand {
+    override async run(ctx: CommandContext) {
+        if (ctx.author.id !== "1252031635692720224")
+            return await ctx.write({
+                content: "Nuh uh"
+            })
+
+        await ctx.deferReply()
+        await ctx.client.cache.messages?.adapter.remove("message.*")
+
+        return await ctx.write({
+            components: new AlertView(await ctx.userTranslations()).successViewCustom("Message cache flushed!"),
+            flags: MessageFlags.IsComponentsV2
+        })
+    }
+}
