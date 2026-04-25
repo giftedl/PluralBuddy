512e11bb8498ba38e2927109419c9a7bf760996e
giftedl <52332868+giftedl@users.noreply.github.com>
2026-04-24 21:09:31 -1000

    fix part 2

--- apps/docs/src/lib/openapi.ts
+++ apps/docs/src/lib/openapi.ts
@@ -1,11 +1,12 @@
 import { createOpenAPI } from 'fumadocs-openapi/server';
-throw new Error(process.cwd())
 
-const schemaPath = process.cwd().includes('docs')
-	? './public/openapi.yml'
-	: './apps/docs/public/openapi.yml';
+const schemaPath = process.cwd().includes('/apps/docs')
+	? `${process.cwd()}/public/openapi.yml`
+	: `${process.cwd()}/apps/docs/public/openapi.yml`;
 
 export const openapi = createOpenAPI({
   // the OpenAPI schema, you can also give it an external URL.
-  input: [schemaPath],
+  input: async () => ({
+    './public/openapi.yml': schemaPath,
+  }),
 });
