6cfb43ded5fc549c82e5670178eee1ff49680be5
giftedl <52332868+giftedl@users.noreply.github.com>
2026-05-03 17:03:50 -0500

    Since wild cards aren't allowed, hard code the 0 for now to make Vercel build

--- apps/docs/src/proxy.ts
+++ apps/docs/src/proxy.ts
@@ -11,5 +11,5 @@ export default function proxy(request: NextRequest, event: NextFetchEvent) {
 }
  
 export const config = {
-  matcher: ['/oauth2/authorize', '/((?!api|image|openapi.yml|sitemap.xml|sitemap-(.*).xml|robots.txt|og|app|.well-known|font|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/oauth2/authorize', '/((?!api|image|openapi.yml|sitemap.xml|sitemap-0.xml|robots.txt|og|app|.well-known|font|_next/static|_next/image|favicon.ico).*)'],
 }
