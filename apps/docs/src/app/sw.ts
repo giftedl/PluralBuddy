fb138935d7712d8a321926a313b372bb8de519ed
giftedl <52332868+giftedl@users.noreply.github.com>
2026-05-04 20:09:41 -0500

    Swap to `NetworkFirst`, try and find ways to make offline PWA's work

--- apps/docs/src/app/sw.ts
+++ apps/docs/src/app/sw.ts
@@ -8,6 +8,7 @@ import {
 	ExpirationPlugin,
 	NetworkFirst,
 	Serwist,
+    StaleWhileRevalidate,
 } from "serwist";
 
 // This declares the value of `injectionPoint` to TypeScript.
@@ -27,22 +28,35 @@ const serwist = new Serwist({
 	skipWaiting: true,
 	clientsClaim: true,
 	navigationPreload: true,
+	precacheOptions: {
+		ignoreURLParametersMatching: [/.*/],
+        fallbackToNetwork: true
+	},
 	runtimeCaching: [
 		{
 			matcher: ({ url }) => url.pathname.startsWith("/app"),
 			handler: new NetworkFirst({
 				cacheName: "app-pages",
-				plugins: [new ExpirationPlugin({ maxEntries: 30, maxAgeSeconds: 60 * 60 * 24 * 30 })],
-				networkTimeoutSeconds: 5,
+				plugins: [
+				],
 			}),
 		},
 		{
 			matcher: ({ url }) => url.pathname.startsWith("/_next/"),
-			handler: new CacheFirst({
+			handler: new NetworkFirst({
 				cacheName: "next-assets",
-				plugins: [new ExpirationPlugin({ maxEntries: 100, maxAgeSeconds: 24 * 60 * 60 })],
+				plugins: [
+				],
 			}),
 		},
+        {
+			matcher: ({ url }) => url.pathname === "/favicon.ico",
+			handler: new NetworkFirst({
+				cacheName: "next-assets",
+				plugins: [
+				],
+			}),
+        },
 		...defaultCache,
 	],
 	fallbacks: {
@@ -65,7 +79,7 @@ const serwist = new Serwist({
 	},
 });
 
-const urlsToCache = ["/_next/", "/app/onboarding", "/manifest.json"] as const;
+const urlsToCache = ["/app/onboarding", "/manifest.json"] as const;
 
 self.addEventListener("install", (event) => {
 	event.waitUntil(
@@ -81,12 +95,31 @@ self.addEventListener("install", (event) => {
 	);
 });
 
-self.addEventListener("fetch", (event) => {
-  const url = new URL(event.request.url);
 
-  if (url.pathname.startsWith("/api/") && url.origin === location.origin) {
-    return Response.json(null);
-  }
-})
+self.addEventListener("fetch", async (event) => {
+	const url = new URL(event.request.url);
+    const cache = await caches.open(serwist.precacheStrategy.cacheName);
+
+    if (url.pathname.startsWith("/app/"))
+	console.log("hello", url);
+    console.log(cache)
+
+	if (url.pathname.startsWith("/api/") && event && url.origin === location.origin) {
+		const tryReq = await fetch(event.request);
+
+        if (!tryReq.ok)
+            event.respondWith(Response.json(null))
+        
+	}
+	if (url.origin === location.origin && url.pathname.startsWith("/app")) {
+        const cacheKey = serwist.getPrecacheKeyForUrl("/app/onboarding");
+        console.log(cacheKey)
+		if (cacheKey) {
+			event.respondWith(
+				(async () => (await cache.match(cacheKey)) ?? Response.error())(),
+			);
+		}
+	}
+});
 
 serwist.addEventListeners();
---
+++ apps/docs/src/components/app/router.tsx
@@ -0,0 +1,175 @@
+"use client";
+
+import AuthorizedAppsPage from "@/components/app/pages/authorized-apps/page";
+import ExpressSpecificAlterPage from "@/components/app/pages/express/page";
+import NotFoundPage from "@/components/app/pages/not-found";
+import { SettingsLayout } from "@/components/app/pages/settings-layout";
+import { DiscordLoginComponent } from "@/components/discord-login";
+import { ExpressList } from "@/components/app/pages/express/express-page.client";
+import { SettingsSidebar } from "@/components/app/settings-sidebar";
+import { Spinner } from "@/components/ui/spinner";
+import { authClient } from "@/lib/auth-client";
+import { TRPCProvider } from "@/server/client";
+import { trpc } from "@/server/client-trpc";
+import {
+	QueryClient,
+	QueryClientProvider,
+	useQuery,
+	useQueryClient,
+} from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import React, {
+	createContext,
+	useCallback,
+	useEffect,
+	useMemo,
+	useState,
+} from "react";
+import {
+	Routes,
+	Route,
+	BrowserRouter,
+	useLocation,
+	Outlet,
+} from "react-router";
+import superjson from "superjson";
+import ImportStagingPage from "@/components/app/pages/import-staging/page";
+import ImportStagingDonePage from "@/components/app/pages/import-staging/done/page";
+import { RemoteSidebarToggle } from "@/components/app/remote-sidebar-toggle";
+import { ThemeToggle } from "@/components/theme-toggle";
+import { AppSettings } from "@/components/app/app-settings";
+import { IndexSettingsAppPage } from "@/components/app/pages/page";
+import WebhooksAppPage from "@/components/app/pages/webhooks/page";
+import OnboardingPage from "@/components/app/pages/onboarding/page";
+import { SystemLayout } from "@/components/app/pages/system-layout";
+import SystemIndexPage from "@/components/app/pages/systems/page";
+import { LoginBoundary } from "@/components/app/pages/login-boundary";
+import EditProfileSystemPage from "@/components/app/pages/systems/edit-profile/page";
+import EditPrivacySystemPage from "@/components/app/pages/systems/edit-privacy/page";
+import SystemPage from "@/components/app/pages/systems/system/page";
+import { PrivacyGroupsSettingsAppPage } from "@/components/app/pages/systems/privacy-groups/page";
+import { gatherPayload, useSyncMutation } from "@/lib/app/use-sync";
+
+declare global {
+	var trpcClient: ReturnType<typeof trpc.createClient>;
+}
+
+const queryClient = new QueryClient();
+
+export default function PluralBuddyRouter() {
+	// const { data: session, isPending } = authClient.useSession();
+	const [trpcClient] = useState(() =>
+		trpc.createClient({
+			links: [
+				httpBatchLink({
+					url: "/api/trpc",
+					transformer: superjson,
+				}),
+			],
+		}),
+	);
+
+
+	// const syncMutation = useSyncMutation(trpcClient);
+	// const { isPending: isSyncPending } = useQuery({
+	// 	queryKey: ["sync"],
+	// 	queryFn: async () =>
+	// 		await syncMutation.mutate({
+	// 			data: await gatherPayload(),
+	// 			prefer: "remote",
+	// 		}),
+	// });
+
+	if (!globalThis.trpcClient) globalThis.trpcClient = trpcClient;
+
+	// if (isPending || isSyncPending)
+	// 	return (
+	// 		<div className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 block justify-center text-center gap-2">
+	// 			<span className="w-full flex justify-center">
+	// 				<Spinner />
+	// 			</span>
+
+	// 			<span className="grid">
+	// 				<span className="text-sm pt-2">{isPending && "Logging in..."}</span>
+	// 				<span className="text-sm pt-2">
+	// 					{isSyncPending && "Syncing with remote..."}
+	// 				</span>
+	// 			</span>
+	// 		</div>
+	// 	);
+
+	if ("virtualKeyboard" in navigator) {
+		(navigator.virtualKeyboard as any).overlaysContent = false;
+	}
+
+	return (
+			<main className="router-boundrary overflow-hidden">
+				<QueryClientProvider client={queryClient}>
+					<TRPCProvider trpcClient={trpcClient} queryClient={queryClient}>
+						<BrowserRouter>
+							<div className="p-2 h-14 w-full bg-sidebar flex justify-between items-center px-4 fixed z-50">
+								<div className="flex flex-row items-center float-left flex-wrap gap-1">
+									<RemoteSidebarToggle />
+								</div>
+								<div className="flex flex-row items-center float-right flex-wrap gap-1">
+									<ThemeToggle />
+									<AppSettings />
+								</div>
+							</div>
+							<div className="h-screen w-screen overflow-hidden">
+								<Routes>
+									<Route path="/app/onboarding" element={<OnboardingPage />} />
+									<Route path="/app/system" element={<SystemLayout />}>
+										<Route index element={<SystemIndexPage />} />
+										<Route path="/app/system/system" element={<SystemPage />} />
+										<Route
+											path="/app/system/edit-profile"
+											element={<EditProfileSystemPage />}
+										/>
+										<Route
+											path="/app/system/edit-privacy"
+											element={<EditPrivacySystemPage />}
+										/>
+										<Route
+											path="/app/system/privacy-groups"
+											element={<PrivacyGroupsSettingsAppPage />}
+										/>
+									</Route>
+									<Route element={<LoginBoundary />}>
+										<Route path="/app/settings" element={<SettingsLayout />}>
+											<Route index element={<IndexSettingsAppPage />} />
+											<Route
+												path="/app/settings/authorized-apps"
+												element={<AuthorizedAppsPage />}
+											/>
+											<Route
+												path="/app/settings/webhooks"
+												element={<WebhooksAppPage />}
+											/>
+											<Route
+												path="/app/settings/express/alter/:alter"
+												element={<ExpressSpecificAlterPage />}
+											/>
+											<Route
+												path="/app/settings/express"
+												element={<ExpressList />}
+											/>
+										</Route>
+										<Route
+											path="/app/import-staging"
+											element={<ImportStagingPage />}
+										/>
+										<Route
+											path="/app/import-staging/done"
+											element={<ImportStagingDonePage />}
+										/>
+									</Route>
+									<Route path="*" element={<NotFoundPage />} />
+								</Routes>
+							</div>
+						</BrowserRouter>
+					</TRPCProvider>
+				</QueryClientProvider>
+			</main>
+	);
+}
