061be3261cad3825d9faa4e2e6eadaab45c9a384
giftedl <52332868+giftedl@users.noreply.github.com>
2026-05-17 14:00:11 -0500

    Fix env variables

--- packages/status-checker/src/api.ts
+++ packages/status-checker/src/api.ts
@@ -2,9 +2,13 @@ import { App, createNodeMiddleware } from "octokit";
 import { createServer } from "node:http";
 
 const app = new App({
-  appId: process.env.CLIENT_ID ?? "",
+  appId: process.env.APP_ID ?? "",
   privateKey: process.env.PRIVATE_KEY ?? "",
   webhooks: { secret: process.env.WEBHOOK_SECRET ?? ""  },
+  oauth: {
+    clientSecret: process.env.CLIENT_SECRET ?? "",
+    clientId: process.env.CLIENT_ID ?? ""
+  }
 });
 
 app.webhooks.on("push", ({ octokit, payload }) => {
