44dd11f6455b18f68f75f1de83f1a2d1bc326156
giftedl <52332868+giftedl@users.noreply.github.com>
2026-05-17 13:52:55 -0500

    Make webhooks for pushing work

--- packages/status-checker/src/api.ts
+++ packages/status-checker/src/api.ts
@@ -1,8 +1,14 @@
-import { App } from "octokit";
+import { App, createNodeMiddleware } from "octokit";
+import { createServer } from "node:http";
 
 const app = new App({
-  appId,
-  privateKey,
-  webhooks: { secret },
+  appId: process.env.CLIENT_ID ?? "",
+  privateKey: process.env.PRIVATE_KEY ?? "",
+  webhooks: { secret: process.env.WEBHOOK_SECRET ?? ""  },
 });
 
+app.webhooks.on("push", ({ octokit, payload }) => {
+  console.log(payload)
+})
+
+createServer(createNodeMiddleware(app)).listen(3000);
