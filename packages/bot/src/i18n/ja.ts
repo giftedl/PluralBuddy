c27648a7204353d73df0898cf6af13e3104d297d
giftedly <52332868+giftedl@users.noreply.github.com>
2026-08-26 01:20:55 -0500

    New translations en.ts (Japanese)
    [ci skip]

--- packages/bot/src/i18n/ja.ts
+++ packages/bot/src/i18n/ja.ts
@@ -62,7 +62,7 @@ SimplyPlural has been discontinued.`,
   IMPORT_TOKEN_DESCRIPTION: "SimplyPlural Token",
   CREATE_NEW_SYS_DESCRIPTION: "Create New System",
   PLURALBUDDY_IMPORT_ERROR_TOO_LARGE: "2j – Importing files cannot be larger than 2MB. Please contact support if you are genuinely trying to import a system larger than 2MB.",
-  ERROR_ATTACHMENT_TOO_LARGE: "2j – Attachments for banners or profile pictures cannot be larger than 1MB.",
+  ERROR_ATTACHMENT_TOO_LARGE: "2j – Uncompressed images must be under 5MB. (After compression, images can only be 1MB.)",
   CREATE_NEW_ALTER_DESCRIPTION: "Create New Alter",
   CREATE_NEW_TAG_DESCRIPTION: "Create New Tag",
   PLURALBUDDY_IMPORT_ERROR_INVALID_JSON: "2k – The file you imported is not valid JSON.",
@@ -701,5 +701,6 @@ Your system is using the front state of **{{ mode }}**.`,
   CONFIGURE_SYSTEM: "Configure System",
   SUCCESSFULLY_DELETED_TAG: "Successfully deleted **{{ tag }}** from your system.",
   WARN_DELETE_TAG: "This tag is associated to over 5 alters, are you sure you'd like to delete this tag from system?",
-  ACKNOWLEDGE_DELETE_TAG: "Confirmed, delete this tag"
+  ACKNOWLEDGE_DELETE_TAG: "Confirmed, delete this tag",
+  AFTER_COMPRESSION_TOO_BIG: "This file is still too big after being compressed and resized. Please make sure a minimal version of this file is smaller than 1MB."
 };
